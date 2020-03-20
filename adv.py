05975ff39656710224e6dcf6f0938a3c3cf0a298
gabeflomo <gabeflomo821@gmail.com>
2020-03-20 12:08:25 -0500

    Last thing to add is BFS

--- adv.py
+++ adv.py
@@ -13,8 +13,9 @@ world = World()
 #map_file = "maps/test_line.txt"
 #map_file = "maps/test_cross.txt"
 #map_file = "maps/test_loop.txt"
-#map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+
+map_file = "maps/test_loop_fork.txt"
+#map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -43,7 +44,57 @@ def valid_direction(new_direction,player):
     directions.remove(delete)
     return directions
 
+def update_graph(graph, player, new_direction, prev_direction):
+    default = '?'
+    possible_exits = {}
+    exits = player.current_room.get_exits()
+    nsew = ['n','s','e','w']
+    room = player.current_room.id
+    
+    for ex in exits:
+        if prev_direction is None:
+            if new_direction == ex:
+                possible_exits[ex] = player.current_room.get_room_in_direction(new_direction).id
+            else:
+                possible_exits[ex] = default
+        else:
+            reverse = reverse_direction(prev_direction)
+            if new_direction == ex:
+                possible_exits[ex] = player.current_room.get_room_in_direction(new_direction).id
+            elif reverse == ex:
+                #reverse = reverse_direction(prev_direction)
+                possible_exits[ex] = player.current_room.get_room_in_direction(reverse).id
+            else:
+                possible_exits[ex] = default
+        
 
+
+
+    # for e in exits:
+    #     # the starting room
+    #     if room == 0:
+    #         # add the room in the new direction
+    #         if new_direction == e:
+
+    #             possible_exits[e] = player.current_room.get_room_in_direction(new_direction).id
+    #         else:
+    #             possible_exits[e] = default
+    #     elif room > 0:
+    #         # get the reverse direction of the previous direction
+    #         reverse = reverse_direction(prev_direction)
+    #         if new_direction == e:
+    #             possible_exits[e] = player.current_room.get_room_in_direction(new_direction).id
+    #         elif prev_direction == e:
+    #             possible_exits[e] = player.current_room.get_room_in_direction(reverse).id
+    #         else:
+    #             possible_exits = default
+
+    graph[room] = possible_exits
+    return graph
+
+
+    
+         
 
 # TRAVERSAL TEST
 visited_rooms = set()
@@ -53,6 +104,7 @@ graph = {}
 rooms_left = len(room_graph) - len(visited_rooms)
 prev_direction = None
 prev_room = None
+
 while rooms_left > 0:
 
     
@@ -75,10 +127,20 @@ while rooms_left > 0:
             traversal_path.append(new_direction)
             updated = True
             print(f"The previous move was {prev_direction} which is invalid to the new direction \nchanging the new direction to {new_direction}")
-        elif player.current_room.get_room_in_direction(new_direction) is None:
-            print("There is no room here")
-
         
+
+    
+    # generate graph    
+    print(update_graph(graph, player, new_direction, prev_direction))
+    
+    # if the player has been to a room do a BFS to find the next room to travel to
+    if player.current_room.get_room_in_direction(new_direction) in visited_rooms:
+        print(f"been to this room")
+        found = False
+        while not found:
+            
+
+
     # move the player
     print(f"moving the player {new_direction}")
     player.travel(new_direction)
@@ -91,13 +153,14 @@ while rooms_left > 0:
     if updated is False:
         traversal_path.append(new_direction)
 
-    #print(f"Updated the traversal path to {traversal_path}")
+    print(f"Updated the traversal path to {traversal_path}")
 
     # update the rooms visited set, how many rooms are left, and the previous direction before the loop
     visited_rooms.add(player.current_room)
     rooms_left = len(room_graph) - len(visited_rooms)
     # rooms_left -= 1
     prev_direction = new_direction
+    
     print()
     
 
